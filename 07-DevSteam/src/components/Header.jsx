e2152cd33952f241a23a5f204494e20eb6f92c47
Ana Geremias <anageremiias@gmail.com>
2025-04-22 10:18:36 -0300

    finalizando header

--- 07-DevSteam/src/components/Header.jsx
+++ 07-DevSteam/src/components/Header.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='w-100 navbar navbar-dark bg-dark justify-content-around align-items-center'>
+     <div id='logo-pesq' className='d-flex gap-5 w-50 justify-content-between'>
+        <div id='logo' role='button' className='d-flex' >
+            <i className='bi bi-controller fs-1 text-light me-3'></i>
+            <span className='navbar-brand fw-bold fs-3'>DevSteam</span>
+        </div>
+        <input type="text"
+        className='w-100 d-none d-md-block border-0 rounded-1 search-input px-4 my-2'
+        placeholder='Buscar...' />
+    </div>
+
+    <div id='carrinho' role='button'>
+        <i className='bi bi-cart  text-light fs-2'></i>
+      
+    </div>
+     </header>
+     
   )
 }
 
