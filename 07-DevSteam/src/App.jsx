f43c18ca81ee3a582a8f75acc670447086989e38
Ana Geremias <anageremiias@gmail.com>
2025-04-23 08:20:42 -0300

    ação de click  dicional +1 item visuais

--- 07-DevSteam/src/App.jsx
+++ 07-DevSteam/src/App.jsx
@@ -1,38 +1,22 @@
-
-import { use } from 'react';
-import './App.css'
-import Header from './components/Header'
-import Promotion from './components/Promotion'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Promotion from "./components/Promotion";
 
 function App() {
-  const[carrinhoItems, setCarrinhoItems] = useState([]);
- const[mostraCarrinho,setMostraCarrinho] = useState(false);
+  const [contador, setContador] = useState(0);
 
- useEffect(() => {
-  const salvaCarrinho = localStorage.getItem('carrinho-devSteam');
-  if (carrinho) {
-    setCarrinhoItems(JSON.parse(salvaCarrinho));
-  }
-
-const adicionarCarrinho = (produto) => {
-  setCarrinhoItems((produtosAnteriores) => {
-    const existing = produtosAnteriores.find((item) => item.id === produto.id);
-    if (existing) {
-      return produtosAnteriores.map((item) =>
-        item.id === produto.id ? { ...item, quantidade: item.quantidade + 1 } : item
-      );
-
-    }else {
-      return [...produtosAnteriores, { ...produto, quantidade: 1 }];
-    }
-});
+  const handleAddCarrinho = () => {
+    setContador(contador + 1);
+  };
 
   return (
     <>
-      <Header contadorJogos={2} />
-      <Promotion/>
+      <Header contadorJogos={contador} />
+      <Promotion onAddCarrinho = {handleAddCarrinho}  //
+       /> 
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 07-DevSteam/src/components/Header.jsx
+++ 07-DevSteam/src/components/Header.jsx
@@ -17,8 +17,8 @@ const Header = (props) => {
 
         <i className='bi bi-cart  text-light fs-2'></i>
 
-        {props.contadorJogod > 0 &&  <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger'>
-            {props.contadorJogod}</span>}
+        {props.contadorJogo > 0 &&  <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger'>
+            {props.contadorJogo}</span>}
         
             
       
